4f11a7a98df59cba6f84274eb14103ebeaa1ce8f
pablo <pablo@codice.es>
2020-04-22 23:45:07 +0200

    initial commit

---
+++ Track.cs
@@ -0,0 +1,103 @@
+﻿using System;
+
+namespace trackrequests
+{
+    class Track
+    {
+        internal Track(
+            int dimensions,
+            int timeColumns,
+            Func<DateTime, DateTime, bool> areSameTimeFunc,
+            Func<DateTime, DateTime, int> unitsBetweenFunc)
+        {
+            mTrack = new int[dimensions, timeColumns];
+            mTotals = new int[dimensions];
+
+            mAreSameTimeFunc = areSameTimeFunc;
+            mUnitsBetweenFunc = unitsBetweenFunc;
+        }
+
+        internal void Operation(
+            DateTime now,
+            int operation,
+            int amount)
+        {
+            ChangeToDifferentTime(now);
+
+            mPrevTime = now;
+            mTrack[operation, mColumn] += amount;
+            mTotals[operation] += amount;
+        }
+
+        /*internal int[] GetTotals()
+        {
+            return mTotals;
+        }*/
+
+        internal int GetTotal(DateTime now)
+        {
+            ChangeToDifferentTime(now);
+
+            mPrevTime = now;
+
+            int result = 0;
+            for (int i = 0; i < mTotals.Length; ++i)
+                result += mTotals[i];
+
+            return result;
+        }
+
+        void ChangeToDifferentTime(DateTime now)
+        {
+            if (mPrevTime == DateTime.MinValue || mAreSameTimeFunc(mPrevTime, now))
+                return;
+
+            int unitsBetween = mUnitsBetweenFunc(mPrevTime, now);
+
+            if (unitsBetween > mTrack.GetLength(1))
+            {
+                // reset
+                mColumn = 0;
+                for (int i = 0; i < mTrack.GetLength(0); ++i)
+                {
+                    mTotals[i] = 0;
+
+                    mTrack[i, mColumn] = 0;
+                }
+
+                return;
+            }
+
+            for (int c = 0; c < unitsBetween; ++c)
+            {
+                ++mColumn;
+
+                if (mColumn == mTrack.GetLength(1))
+                {
+                    mbFull = true;
+
+                    mColumn = 0;
+                }
+
+                if (mbFull)
+                {
+                    for (int i = 0; i < mTrack.GetLength(0); ++i)
+                    {
+                        mTotals[i] -= mTrack[i, mColumn];
+
+                        mTrack[i, mColumn] = 0;
+                    }
+                }
+            }
+        }
+
+        DateTime mPrevTime = DateTime.MinValue;
+        int mColumn = 0;
+        int[,] mTrack;
+        int[] mTotals;
+        bool mbFull = false;
+
+        Func<DateTime, DateTime, bool> mAreSameTimeFunc;
+        Func<DateTime, DateTime, int> mUnitsBetweenFunc;
+    }
+}
---
+++ TrackTests.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using NUnit.Framework;
+
+namespace trackrequests
+{
+    [TestFixture]
+    class TrackTests
+    {
+        [Test]
+        public void TestBasicTrack()
+        {
+            Track opsLast5seconds = new Track(
+                130,
+                5,
+                (one, two) =>
+                {
+                    return one.Year == two.Year
+                        && one.Month == two.Month
+                        && one.Day == two.Day
+                        && one.Hour == two.Hour
+                        && one.Minute == two.Minute
+                        && one.Second == two.Second;
+                },
+                (one, two) =>
+                {
+                    return (int) (two - one).TotalSeconds;
+                });
+
+            DateTime start = new DateTime(2020, 04, 24, 16, 00, 00);
+
+            opsLast5seconds.Operation(start.AddSeconds(1), 120, 1);
+            opsLast5seconds.Operation(start.AddSeconds(1), 120, 1);
+            opsLast5seconds.Operation(start.AddSeconds(1), 121, 1);
+            opsLast5seconds.Operation(start.AddSeconds(1), 122, 1);
+            opsLast5seconds.Operation(start.AddSeconds(1), 122, 1);
+
+            opsLast5seconds.Operation(start.AddSeconds(2), 120, 1);
+            opsLast5seconds.Operation(start.AddSeconds(2), 121, 1);
+            opsLast5seconds.Operation(start.AddSeconds(2), 122, 1);
+            opsLast5seconds.Operation(start.AddSeconds(2), 122, 1);
+
+            opsLast5seconds.Operation(start.AddSeconds(3), 120, 1);
+
+            opsLast5seconds.Operation(start.AddSeconds(4), 120, 1);
+            opsLast5seconds.Operation(start.AddSeconds(4), 121, 1);
+            opsLast5seconds.Operation(start.AddSeconds(4), 122, 1);
+
+            opsLast5seconds.Operation(start.AddSeconds(5), 121, 1);
+            opsLast5seconds.Operation(start.AddSeconds(5), 122, 1);
+
+            Assert.AreEqual(15, opsLast5seconds.GetTotal(start.AddSeconds(5)));
+
+            opsLast5seconds.Operation(start.AddSeconds(6), 121, 1);
+
+            Assert.AreEqual(11, opsLast5seconds.GetTotal(start.AddSeconds(6)));
+
+            Assert.AreEqual(6, opsLast5seconds.GetTotal(start.AddSeconds(8)));
+
+            Assert.AreEqual(0, opsLast5seconds.GetTotal(start.AddSeconds(120)));
+
+            opsLast5seconds.Operation(start.AddSeconds(200), 120, 1);
+            opsLast5seconds.Operation(start.AddSeconds(200), 120, 1);
+
+            Assert.AreEqual(2, opsLast5seconds.GetTotal(start.AddSeconds(201)));
+        }
+    }
+}
