0574f499b02f38f53297191012a4f626d03dfd1c
asanlua <mail@asanlua.com>
2020-04-23 21:06:34 +0200

    Added all the tests

--- TrackRequestsTests/TrackTests.cs
+++ TrackRequestsTests/TrackTests.cs
@@ -76,4 +76,83 @@ namespace TrackRequestsTests
             Assert.AreEqual(2, opsLast5Seconds.GetTotal(start.AddSeconds(201)));
         }
     }
+    
+    [Test]
+        public void TestMinutesAndHours()
+        {
+            Track opsLastMinute = new Track(
+                130,
+                60,
+                (one, two) =>
+                {
+                    return one.Year == two.Year
+                        && one.Month == two.Month
+                        && one.Day == two.Day
+                        && one.Hour == two.Hour
+                        && one.Minute == two.Minute
+                        && one.Second == two.Second;
+                },
+                (one, two) =>
+                {
+                    return (int)Math.Round((two - one).TotalSeconds);
+                });
+
+            Track opsLastHour = new Track(
+                130,
+                60,
+                (one, two) =>
+                {
+                    return one.Year == two.Year
+                        && one.Month == two.Month
+                        && one.Day == two.Day
+                        && one.Hour == two.Hour
+                        && one.Minute == two.Minute;
+                },
+                (one, two) =>
+                {
+                    return (int)Math.Round((two - one).TotalMinutes);
+                });
+
+
+            DateTime start = new DateTime(2020, 04, 24, 16, 00, 00);
+
+            TrackOp(opsLastMinute, opsLastHour, start.AddSeconds(1), 120, 5);
+
+            TrackOp(opsLastMinute, opsLastHour, start.AddSeconds(10), 120, 3);
+
+            TrackOp(opsLastMinute, opsLastHour, start.AddSeconds(35), 121, 2);
+
+            Assert.AreEqual(10, opsLastMinute.GetTotal(start.AddSeconds(35)));
+            Assert.AreEqual(10, opsLastHour.GetTotal(start.AddSeconds(35)));
+
+            TrackOp(opsLastMinute, opsLastHour, start.AddSeconds(200), 120, 1);
+            TrackOp(opsLastMinute, opsLastHour, start.AddSeconds(200), 120, 1);
+
+            Assert.AreEqual(2, opsLastMinute.GetTotal(start.AddSeconds(200)));
+            Assert.AreEqual(12, opsLastHour.GetTotal(start.AddSeconds(200)));
+
+            TrackOp(opsLastMinute, opsLastHour, start.AddMinutes(30), 120, 1);
+            TrackOp(opsLastMinute, opsLastHour, start.AddMinutes(30), 120, 1);
+
+            Assert.AreEqual(2, opsLastMinute.GetTotal(start.AddMinutes(30)));
+            Assert.AreEqual(14, opsLastHour.GetTotal(start.AddMinutes(30)));
+
+            TrackOp(opsLastMinute, opsLastHour, start.AddMinutes(58), 120, 2);
+            TrackOp(opsLastMinute, opsLastHour, start.AddMinutes(58), 120, 2);
+
+            Assert.AreEqual(4, opsLastMinute.GetTotal(start.AddMinutes(58)));
+            Assert.AreEqual(18, opsLastHour.GetTotal(start.AddMinutes(58)));
+
+            TrackOp(opsLastMinute, opsLastHour, start.AddMinutes(61), 120, 2);
+
+            Assert.AreEqual(2, opsLastMinute.GetTotal(start.AddMinutes(61)));
+            Assert.AreEqual(10, opsLastHour.GetTotal(start.AddMinutes(61)));
+        }
+
+        static void TrackOp(Track one, Track two, DateTime d, int op, int count)
+        {
+            one.Operation(d, op, count);
+            two.Operation(d, op, count);
+        }
+    }
 }
